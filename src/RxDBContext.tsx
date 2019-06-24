2c61d26bbe7dc3be2f77fa0a45e827abd4ee5de1
Milan Proell <milan.proell@googlemail.com>
2019-06-24 14:33:35 +0200

    Migarting to setDbFnc hook

--- src/RxDBContext.tsx
+++ src/RxDBContext.tsx
@@ -1,49 +1,70 @@
-import React, {
-  ReactElement,
-  ReactNode,
-  useContext,
-  useEffect,
-  useState,
-} from 'react';
+import React, { ReactElement, ReactNode, useContext, useState } from 'react';
 import { RxCollection, RxDatabase } from 'rxdb';
 
-const RxDBContext = React.createContext<null | RxDatabase<any>>(null);
+type setDbPromiseFNC<T> = (dbPromise?: Promise<RxDatabase<T>>) => void;
+
+type IRxDBContext<
+  T extends { [key: string]: RxCollection } = { [key: string]: RxCollection }
+> = {
+  db: RxDatabase<T> | null;
+  setDbPromise?: setDbPromiseFNC<T>;
+};
+
+const RxDBContext = React.createContext<IRxDBContext<any>>({ db: null });
 
 export interface RxDBProviderProps<Collections> {
   readonly children?: ReactNode;
-  readonly db: RxDatabase<Collections>;
+  readonly db: RxDatabase<Collections> | null;
 }
 
-export interface RxDBProviderWithPromiseProps<Collections> {
-  readonly children?: ReactNode;
-  readonly dbPromise?: Promise<RxDatabase<Collections>>;
-}
-
-export function RxDBProvider<Collections = { [key: string]: RxCollection }>({
-  dbPromise,
+export function RxDBProvider<
+  Collections extends { [key: string]: RxCollection } = {
+    [key: string]: RxCollection;
+  }
+>({
   children,
-}: RxDBProviderWithPromiseProps<Collections>): ReactElement<
-  RxDBProviderProps<Collections>
-> {
+}: {
+  children?: ReactNode;
+}): ReactElement<RxDBProviderProps<Collections>> {
   const [db, setDb] = useState<RxDatabase<Collections> | null>(null);
-  useEffect(() => {
+  const setDbPromise: setDbPromiseFNC<Collections> = (
+    dbPromise?: Promise<RxDatabase<Collections>>
+  ) => {
     if (dbPromise) {
-      dbPromise.then(resolvedDb => setDb(resolvedDb));
+      dbPromise.then(resolvedDb => {
+        setDb(resolvedDb);
+      });
     } else {
       setDb(null);
     }
-  }, [dbPromise]);
-  return <RxDBContext.Provider value={db}>{children}</RxDBContext.Provider>;
+  };
+  return (
+    <RxDBContext.Provider value={{ db, setDbPromise }}>
+      {children}
+    </RxDBContext.Provider>
+  );
 }
 
-export function useRxDB<Collections = { [key: string]: RxCollection }>(
-  overrideDb?: RxDatabase<Collections>
-): null | RxDatabase<Collections> {
-  const db = useContext<null | RxDatabase<Collections>>(RxDBContext);
+export function useRxDB<
+  Collections extends { [key: string]: RxCollection } = {
+    [key: string]: RxCollection;
+  }
+>(overrideDb?: RxDatabase<Collections>): null | RxDatabase<Collections> {
+  const { db } = useContext<IRxDBContext<Collections>>(RxDBContext);
 
   if (overrideDb) {
     return overrideDb;
   }
 
   return db;
+}
+
+export function useSetRxDB<
+  Collections extends { [key: string]: RxCollection } = {
+    [key: string]: RxCollection;
+  }
+>(): setDbPromiseFNC<Collections> | undefined {
+  const { setDbPromise } = useContext<IRxDBContext<Collections>>(RxDBContext);
+
+  return setDbPromise;
 }
